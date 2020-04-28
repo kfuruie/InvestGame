14c2d07bba498681b8db404782e11966746ebcac
ledaniel1020 <dbl3jf@virginia.edu>
2020-04-27 21:10:34 -0400

    Update investform.php

---
+++ investform.php
@@ -0,0 +1,14 @@
+<?php
+header('Access-Control-Allow-Origin: http://localhost:4200');
+// header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
+
+$postdata = file_get_contents("php://input")
+
+$request = json_decode($postdata);
+
+echo($request);
+
+?>
